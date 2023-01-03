77bf925b0f90d2c64e21b7951199bbf098445a2f
HANJANGWON <wkddnjsdl00@naver.com>
2023-01-03 20:32:26 +0900

    add : pagination posts in home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,7 @@ import FEED_QUERY from "../documents/queries/seeFeed.query";
 import { AnimatePresence } from "framer-motion";
 import { useMatch, PathMatch } from "react-router-dom";
 import UploadPost from "./UploadPost";
+import { useCallback, useEffect } from "react";
 
 const Home = () => {
   const uploadPostPathMath: PathMatch<string> | null =
@@ -14,11 +15,21 @@ const Home = () => {
       offset: 0,
     },
   });
-  fetchMore({
-    variables: {
-      offset: data?.seeFeed?.length,
-    },
-  });
+
+  //pagination posts
+  const handleScroll = useCallback(async (): Promise<void> => {
+    const scrollTop: number = document.documentElement.scrollTop;
+    const innerHeight: number = window.innerHeight;
+    const scrollHeight: number = document.body.scrollHeight;
+    if (scrollTop + innerHeight >= scrollHeight) {
+      await fetchMore({ variables: { offset: data?.seeFeed?.length } });
+    }
+  }, [data, fetchMore]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [handleScroll]);
 
   return (
     <div>
